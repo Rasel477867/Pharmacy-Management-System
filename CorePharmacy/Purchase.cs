dd855e4e05e91ef3bbe95a8581925e0076bbca6f
“Rasel477867” <raselaye3737@gamil.com>
2025-01-29 13:38:51 +0600

    Table design and Medicine index completed.

---
+++ CorePharmacy/Purchase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CorePharmacy
+{
+    public class Purchase
+    {
+        public int Id { get; set; }
+        public string? RequestBy { get; set; }
+        public string? PurchaseStore { get; set; }
+        public string? Currency { get; set; }
+        public string? Priority { get; set; }
+        public string? Remarks { get; set; }
+        public string? Status { get; set; }
+        public DateTime RequiredDate { get; set; }
+    }
+}
---
+++ CorePharmacy/PurchaseMedicine.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CorePharmacy
+{
+    public class PurchaseMedicine
+    {
+        public int Id { get; set; }
+        public string? MedicineName { get; set; }
+        public decimal PurchasePrice { get; set; }
+        public decimal SellingPrice { get; set; }
+        public string? SKU { get; set; }
+        public DateTime ExpiryDate { get; set; }
+        public int StockQuantity { get; set; }
+        public int PurchaseId { get; set; }
+    }
+}
